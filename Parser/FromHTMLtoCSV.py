d61ac9f262711bf20a5ec13684449aa9530761f1
Anastasia <nastasy.kozh@gmail.com>
2023-02-17 02:24:18 +0300

    Editing code

--- Parser/FromHTMLtoCSV.py
+++ Parser/FromHTMLtoCSV.py
@@ -9,16 +9,16 @@ page = BeautifulSoup(link.text, 'lxml')
 tables = page.find('table', class_='confluenceTable')
 
 headers = []
-for name in tables.find_all('th'):
-    header = name.text
-    headers.append(header)
-print(headers)
-
 lst = []
 num = []
 desc = []
 i = 0
 k = 1
+
+for name in tables.find_all('th'):
+    header = name.text
+    headers.append(header)
+print(headers)
 
 for name in tables.find_all('td'):
     status = name.text
@@ -29,7 +29,7 @@ while i < int(len(lst)):
     num.append(lst[i])
     i += 2
 print(num)
-#
+
 while k < int(len(lst)):
     desc.append(lst[k])
     k += 2
@@ -37,7 +37,7 @@ print(desc)
 
 data = [num, desc]
 export = zip_longest(*data, fillvalue='')
-with open('PythonPractice/Parser/Codes.csv', 'w', newline='') as file:
+with open('Codes.csv', 'w', newline='') as file:
     writer = csv.writer(file)
     writer.writerow(headers)
     writer.writerows(export)
